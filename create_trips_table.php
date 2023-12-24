b0ce576085d8451354447ae9262649e56f0eef5a
TasnimHoque <83399964+TasnimHoque@users.noreply.github.com>
2023-12-24 14:22:47 +0600

    create_trips_table.php

---
+++ create_trips_table.php
@@ -0,0 +1,22 @@
+// database/migrations/xxxx_xx_xx_create_trips_table.php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
